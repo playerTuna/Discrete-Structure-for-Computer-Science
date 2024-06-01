213f29cbc089f823e199c5cd38f85c81682934fa
Tien Nam Vo <145256269+TunaLikeSweet@users.noreply.github.com>
2024-06-01 19:54:00 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,63 +3,61 @@
 
 int main()
 {
-    ifstream fin;
-    int G[20][20];
-    int G2[20][20];
-    int BFValue[20];
-    int BFPrev[20];
-    fin.open("C:\\Users\\Nem\\VNU-HCMUT\\Discrete Structure for Computer Science - CO1007\\inMat1.txt");
-    int n = 8;
-    for (int i = 0; i < n; i++)
+    // ifstream fin;
+    // int G[20][20];
+    // int G2[20][20];
+    // int BFValue[20];
+    // int BFPrev[20];
+    // fin.open("C:\\Users\\Nem\\VNU-HCMUT\\Discrete Structure for Computer Science - CO1007\\inMat1.txt");
+    // int n = 8;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     BFValue[i] = -1;
+    //     BFPrev[i] = -1;
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         fin >> G[i][j];
+    //     }
+    // }
+    // fin.close();
+    // fin.open("C:\\Users\\Nem\\VNU-HCMUT\\Discrete Structure for Computer Science - CO1007\\inMat2.txt");
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         fin >> G2[i][j];
+    //     }
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (i % 2)
+    //         BF(G, n, 'D', BFValue, BFPrev);
+    //     else
+    //         BF(G2, n, 'D', BFValue, BFPrev);
+    //     cout << "step" << i << ":" << endl;
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << BFValue[j] << " ";
+    //     }
+    //     cout << endl;
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << BFPrev[j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << BF_Path(G, n, 'A', 'E') << endl;
+    // fin.close();
+    ifstream file("621.txt");
+    int Graph[20][20];
+    if (!file.is_open())
     {
-        BFValue[i] = -1;
-        BFPrev[i] = -1;
-        for (int j = 0; j < n; j++)
-        {
-            fin >> G[i][j];
-        }
+        cout << "Can't open file" << endl;
+        return 0;
     }
-    fin.close();
-    fin.open("C:\\Users\\Nem\\VNU-HCMUT\\Discrete Structure for Computer Science - CO1007\\inMat2.txt");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            fin >> G2[i][j];
-        }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (i % 2)
-            BF(G, n, 'D', BFValue, BFPrev);
-        else
-            BF(G2, n, 'D', BFValue, BFPrev);
-        cout << "step" << i << ":" << endl;
-        for (int j = 0; j < n; j++)
-        {
-            cout << BFValue[j] << " ";
-        }
-        cout << endl;
-        for (int j = 0; j < n; j++)
-        {
-            cout << BFPrev[j] << " ";
-        }
-        cout << endl;
-    }
-    cout << BF_Path(G, n, 'A', 'E') << endl;
-    fin.close();
+    for (int i = 0; i < 20; i++)
+        for (int j = 0; j < 20; j++)
+            file >> Graph[i][j];
+    string output = Traveling(Graph, 12, 'A');
+    cout << output << endl;
 }
-
-// int main()
-// {
-//     int Graph[20][20] = {
-//         {0, 2, 9, 7, 5},
-//         {2, 0, 6, 8, 4},
-//         {9, 6, 0, 3, 9},
-//         {7, 8, 3, 0, 6},
-//         {5, 4, 9, 6, 0}
-//     };
-//     int num_vertices = 5;
-//     char start_vertex = 'A';
-//     cout << Traveling(Graph, num_vertices, start_vertex);
-// }
