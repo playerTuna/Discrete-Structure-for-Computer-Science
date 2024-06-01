277f9b74c5eebc10a301e3c593b531d86d2e6e87
Tien Nam Vo <145256269+TunaLikeSweet@users.noreply.github.com>
2024-06-01 17:57:34 +0700

    Update tsp.cpp

--- tsp.cpp
+++ tsp.cpp
@@ -1,15 +1,8 @@
 #include "tsp.h"
 
-#include <climits>
-#include <cstring>
-int num_vertex;
-int cost[21][21];
-int X[21];
-int visited[21];
-int dist = 0;
-int result = INT_MAX;
+vector<int> bestPath;
 
-void TSP(int i)
+void TSP(int i, int num_vertex, vector<int>& X, vector<int>& visited, vector<vector<int>>& cost, int& dist, int& result)
 {
     for (int j = 1; j <= num_vertex; j++)
         if (!visited[j]) // if vertex j is not visited yet
@@ -23,37 +16,51 @@ void TSP(int i)
                 {
                     // Update result:
                     result = min(result, temp_dist + cost[X[num_vertex]][X[1]]);
+                    bestPath = X;
                 }
                 else
                 {
                     dist = temp_dist;
-                    TSP(i + 1);
-                    dist -= cost[X[i - 1]][X[i]];
+                    TSP(i + 1, num_vertex, X, visited, cost, dist, result);
+                    // dist -= cost[X[i - 1]][X[i]];
                 }
             }
             // Backtrack:
             visited[j] = 0;
         }
 }
+
 string Traveling(int Graph[][20], int num_vertex, char start)
 {
-    ::num_vertex = num_vertex;
+    vector<vector<int>> cost(num_vertex+1, vector<int>(num_vertex+1));
     for (int i = 0; i < num_vertex; i++)
         for (int j = 0; j < num_vertex; j++)
             cost[i + 1][j + 1] = Graph[i][j];
+    for (int i = 1; i <= num_vertex; i++)
+    {
+        for (int j = 1; j <= num_vertex; j++)
+        {
+            cout << cost[i][j] << " ";
+        }
+        cout << "\n";
+    }
     int start_vertex = start - 'A' + 1;
-    memset(visited, 0, sizeof(visited)); // Reset visited array
-    memset(X, 0, sizeof(X));             // Reset X array
-    dist = 0;
-    result = INT_MAX;
+    vector<int> visited(num_vertex+1, 0); // Reset visited array
+    vector<int> X(num_vertex+1, 0);       // Reset X array
+    int dist = 0;
+    int result = INT_MAX;
     visited[start_vertex] = 1;
     X[1] = start_vertex;
-    TSP(2);
+    TSP(2, num_vertex, X, visited, cost, dist, result);
     string res = "";
     for (int i = 1; i <= num_vertex; i++)
-        res += char(X[i] + 'A' - 1);
-    res += char(X[1] + 'A' - 1);
+    {
+        res += char(bestPath[i] + 'A' - 1);
+        res += " ";
+    }
+    res += char(bestPath[1] + 'A' - 1);
     res += " ";
     res += to_string(result);
+    cout << res;
     return res;
 }
