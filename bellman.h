88738926a7ef37c5594bc70a8ab8c2747057eb8e
Tien Nam Vo <145256269+TunaLikeSweet@users.noreply.github.com>
2024-06-06 18:02:31 +0700

    Update bellman.h

--- bellman.h
+++ bellman.h
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <limits>
 #include <climits>
 #include <vector>
 #include <algorithm>
 #include <fstream>
 #include <sstream>
-#include <queue>
 using namespace std;
 
 #ifndef BELLMAN_H
@@ -21,11 +19,7 @@ using namespace std;
  *  prev_array: Mảng lưu trữ đỉnh trước của mỗi đỉnh
  * ***********************************************************/
 
-struct Edge
-{
-    int u, v, weight;
-};
-void BF(int Graph[][20], int num_vertices, char start_vertex, int value_array[], int prev_array[]);
-string BF_Path(int Graph[][20], int num_vertices, char start_vertex, char goal_vertex);
+void BF(int Graph[20][20], int num_vertices, char start_vertex, int value[], int prev[]);
+string BF_Path(int Graph[20][20], int num_vertices, char start_vertex, char goal_vertex);
 
 #endif // BELLMAN_H
